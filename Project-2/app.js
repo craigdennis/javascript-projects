61c749a410588cf1e84f08675a8dadc64dae5cd1
Unknown <craigdennis1990@gmail.com>
2020-02-05 06:26:10 +0000

    let and const
    updated variable and constants to be let and const

--- Project-2/app.js
+++ Project-2/app.js
@@ -1,66 +1,61 @@
-var btn, hexCodeLetters, letterArr, numberArr, hexCode, backgroundCol;
+let letterArr, numberArr, hexCode, backgroundCol;
 
-btn = document.getElementById('btn');
-
+const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
-    backgroundCol = shuffle(createHexCode());
-    document.body.style.backgroundColor = '#' + backgroundCol;
-    document.getElementById('hex-code').textContent = backgroundCol;
-    
+  backgroundCol = shuffle(createHexCode());
+  document.body.style.backgroundColor = '#' + backgroundCol;
+  document.getElementById('hex-code').textContent = backgroundCol;
 });
 
 //produces a random number
 
 function randomNumber(range) {
-    return Math.floor(Math.random() * range);
+  return Math.floor(Math.random() * range);
 }
 
 //function to create a hexcode
 
 function createHexCode() {
-    hexCodeLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
-    letterArr = [];
-    numberArr = [];
-    hexCode = [];
+  const hexCodeLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
+  letterArr = [];
+  numberArr = [];
+  hexCode = [];
 
+  for (let i = 0; i < 6; i++) {
+    letterArr.push(hexCodeLetters[randomNumber(6)]);
+  }
 
-    for (var i = 0; i < 6; i++) {
-        letterArr.push(hexCodeLetters[randomNumber(6)]);
-    }
+  for (let i = 0; i < 6; i++) {
+    numberArr.push(randomNumber(6));
+  }
 
-    for (var i = 0; i < 6; i++) {
-        numberArr.push(randomNumber(6));
-    }
-
-    for (var i = 0; i < 3; i++) {
-        hexCode.push(letterArr.pop());
-        hexCode.push(numberArr.pop());
-
-    }
-    return hexCode;
+  for (let i = 0; i < 3; i++) {
+    hexCode.push(letterArr.pop());
+    hexCode.push(numberArr.pop());
+  }
+  return hexCode;
 }
 
 //function to shuffle array to randomize the hexcode
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      randomIndex = randomNumber(currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array.join('');
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex = randomNumber(currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
 
-
+  return array.join('');
+}
