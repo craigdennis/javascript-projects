689b06e9b3f0a5c31088568fe7ad3b53c2547c0c
Unknown <craigdennis1990@gmail.com>
2020-03-04 06:36:08 +0000

    Project 4 - HTML
    started project 4, created the html ready for styling

---
+++ Project-4/app.js
@@ -0,0 +1,11 @@
+/*
+
+The Brief - 
+
+After you enter text into the input box, upon pressing the “Enter” key, your message should show up in the “Last Message Delivered” Section.
+After the “Enter” key is pressed, the text box value should return to an empty text box.
+If no input is submitted, an alert should show (using the ‘show' class in the CSS file) in the “Last Message Delivered” section and then disappear after 2 second.
+
+*/
+
+
