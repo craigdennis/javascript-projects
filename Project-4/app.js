85d7872966dc32715c1e500bbc04f4590e7a0ef1
Unknown <craigdennis1990@gmail.com>
2020-03-05 19:43:54 +0000

    project 4 completed
    finished off project 4.

--- Project-4/app.js
+++ Project-4/app.js
@@ -1,11 +1,25 @@
-/*
+const inputSelector = document.getElementById("input");
 
-The Brief - 
+input.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    getInput();
+  }
+});
 
-After you enter text into the input box, upon pressing the “Enter” key, your message should show up in the “Last Message Delivered” Section.
-After the “Enter” key is pressed, the text box value should return to an empty text box.
-If no input is submitted, an alert should show (using the ‘show' class in the CSS file) in the “Last Message Delivered” section and then disappear after 2 second.
+document.getElementById('btn').addEventListener('click', function() {
+  getInput();
+});
 
-*/
-
-
+function getInput() {
+    if (inputSelector.value === "") {
+        document.querySelector('h3').textContent = "Please enter a message";
+        setTimeout(function(){
+            document.getElementById("message").innerHTML = 'Last Message Delivered';
+        }, 3000);
+    } else {
+        const input = inputSelector.value;
+        document.querySelector('h3').textContent = input;
+        inputSelector.value = "";
+    }
+ 
+}
