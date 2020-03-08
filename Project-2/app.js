a54d28164d6ce3533dabe8fed8b19d5839726e3a
Unknown <craigdennis1990@gmail.com>
2020-03-08 18:24:04 +0000

    project 2 revisited
    with help from the list that im working on, refactored the code

--- Project-2/app.js
+++ Project-2/app.js
@@ -1,61 +1,16 @@
-let letterArr, numberArr, hexCode, backgroundCol;
-
+const hexCodeLetters = ['0','1','2','3','4','5','6','7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function() {
-  backgroundCol = shuffle(createHexCode());
-  document.body.style.backgroundColor = '#' + backgroundCol;
-  document.getElementById('hex-code').textContent = backgroundCol;
-});
-
-//produces a random number
-
-function randomNumber(range) {
-  return Math.floor(Math.random() * range);
-}
-
-//function to create a hexcode
+btn.addEventListener('click', createHexCode);
 
 function createHexCode() {
-  const hexCodeLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
-  letterArr = [];
-  numberArr = [];
-  hexCode = [];
-
+  let hexcode = "#";
   for (let i = 0; i < 6; i++) {
-    letterArr.push(hexCodeLetters[randomNumber(6)]);
+    let index = Math.floor(Math.random() * hexCodeLetters.length);
+    hexcode += hexCodeLetters[index];
+  
   }
 
-  for (let i = 0; i < 6; i++) {
-    numberArr.push(randomNumber(6));
-  }
-
-  for (let i = 0; i < 3; i++) {
-    hexCode.push(letterArr.pop());
-    hexCode.push(numberArr.pop());
-  }
-  return hexCode;
-}
-
-//function to shuffle array to randomize the hexcode
-
-function shuffle(array) {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    randomIndex = randomNumber(currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array.join('');
+  document.body.style.backgroundColor = hexcode;
+  document.getElementById('hex-code').textContent = hexcode;
 }
