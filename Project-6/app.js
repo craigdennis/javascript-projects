c5c3d9a669e39c1f46c4a0d36dab2daee26c1e8b
Unknown <craigdennis1990@gmail.com>
2020-03-11 07:10:46 +0000

    project 6
    refactored into one function

--- Project-6/app.js
+++ Project-6/app.js
@@ -1,21 +1,21 @@
 const dogImage = ['dog0', 'dog1', 'dog2', 'dog3', 'dog4'];
 let currentIndex = 0;
 
-document.getElementById('left').addEventListener('click', leftClicked);
-document.getElementById('right').addEventListener('click', rightClicked);
+document.getElementById('left').addEventListener('click', function() { buttonClicked('left'); });
+document.getElementById('right').addEventListener('click', function() { buttonClicked('right'); });
 
-function rightClicked() {
-    currentIndex++;
-    if (currentIndex > dogImage.length -1 ) {
-        currentIndex = 0;   
-    }
-    document.getElementById('image').src = 'images/' + dogImage[currentIndex] + '.jpg';
-}
-
-function leftClicked() {
+function buttonClicked(btn) {
+  if (btn === 'left') {
     currentIndex--;
     if (currentIndex < 0) {
-        currentIndex = dogImage.length - 1;
+      currentIndex = dogImage.length - 1;
     }
-    document.getElementById('image').src = 'images/' + dogImage[currentIndex] + '.jpg';
+  }
+  if (btn === 'right') {
+    currentIndex++;
+    if (currentIndex > dogImage.length - 1) {
+      currentIndex = 0;
+    }
+  }
+  document.getElementById('image').src = 'images/' + dogImage[currentIndex] + '.jpg';
 }
