cd0bd1d71da6220ed8d8c1133c371518fcd4fb4b
Unknown <craigdennis1990@gmail.com>
2020-03-14 18:00:50 +0000

    project 7 complete
    project 7 complete. no major design styling and not mobile responsive

--- Project-7/app.js
+++ Project-7/app.js
@@ -1,11 +1,13 @@
+let currentIndex = 0;
 const customerFeedback = [
-  new Feedback('Bob', 'person1.jpeg', '4-star.png', 'This was awesome!'),
-  new Feedback('Jane','person2.jpeg','5-star.png','This was more than awesome!'),
-  new Feedback('Jamie', 'person3.jpeg', '1-star.png', 'This was propa shet!'),
-  new Feedback('Flexicity', 'person4.jpeg', '2-star.png', 'This was shet!')
+  new Feedback('Corona-verus', 'person1.jpeg', '4-star.png', 'This was awesome!'),
+  new Feedback('Bob','person2.jpeg','5-star.png','This was more than awesome!'),
+  new Feedback('Emma', 'person3.jpeg', '1-star.png', 'This was propa shet!'),
+  new Feedback('Flex-dem-Guns', 'person4.jpeg', '2-star.png', 'This was shet!')
 ];
 
-let currentIndex = 0;
+insertData(customerFeedback, 0);
+
 
 document.getElementById('left').addEventListener('click', function() {
   buttonClicked('left');
@@ -28,12 +30,7 @@ function buttonClicked(btn) {
       currentIndex = 0;
     }
   }
-  document.getElementById('customer-image').src =
-    'images/' + customerFeedback[currentIndex].image;
-  document.getElementById('name').textContent =
-    customerFeedback[currentIndex].name;
-    document.getElementById('star').src = 'images/' + customerFeedback[currentIndex].stars;
-    document.getElementById('text').textContent = customerFeedback[currentIndex].text;
+  insertData(customerFeedback, currentIndex);
 
 }
 
@@ -42,6 +39,13 @@ function Feedback(name, image, stars, text) {
   this.image = image;
   this.stars = stars;
   this.text = text;
+}
+
+function insertData(feedback, index) {
+  document.getElementById('customer-image').src ='images/' + feedback[index].image;
+  document.getElementById('name').textContent = feedback[index].name;
+  document.getElementById('star').src = 'images/' + feedback[index].stars;
+  document.getElementById('text').textContent = feedback[index].text;
 }
 
 
